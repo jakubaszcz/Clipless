47174403dc78f217bb08213378ffa365bb884e4d
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-04-07 23:49:58 +0200

    feat: implement the database method basis

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use global_hotkey::{GlobalHotKeyEvent, GlobalHotKeyManager, hotkey::{HotKey, Modifiers, Code}};
 use winit::event_loop::EventLoop;
 use selection::get_text;
+use rusqlite::{Connection, Result};
+
 
 fn main() {
     let event_loop = EventLoop::new().unwrap();
     let manager = GlobalHotKeyManager::new().unwrap();
+
+    let connection = init_database().unwrap();
 
     let custom_copy_hotkey = HotKey::new(
         Some(Modifiers::CONTROL | Modifiers::ALT),
@@ -18,7 +22,26 @@ fn main() {
     event_loop.run(move |_event, _| {
         if let Ok(_event) = receiver.try_recv() {
             let selected_text = get_text();
-            println!("{}", selected_text);
+
+            insert_clip(&connection, &selected_text);
         }
     }).unwrap();
+}
+
+fn init_database() -> Result<Connection> {
+    let conn = Connection::open("clipboard.db")?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS clips (
+            id INTEGER PRIMARY KEY,
+            content TEXT NOT NULL
+        )",
+        [],
+    )?;
+
+    Ok(conn)
+}
+
+fn insert_clip(conn: &Connection, content: &str) {
+    conn.execute("INSERT INTO clips (content) VALUES (?)", &[content]).unwrap();
 }
